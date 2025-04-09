14b658bd8078aacf420f0b760b18888f071297dc
stephentyers1975 <steve.tyers@gmail.com>
2025-04-09 16:09:15 +0100

    updated terraform provider versions

--- src/terraform/infra/versions.tf
+++ src/terraform/infra/versions.tf
@@ -2,11 +2,13 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.75.0"
+      # version = "~> 3.75.0"
+      version = "~> 4.26.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 2.45.0"
+      # version = "~> 2.45.0"
+      version = "~> 2.7.0"
     }
   }
   backend "azurerm" {
