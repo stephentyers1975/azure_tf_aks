b445625e5a65a5ab92172f99b6e9709c10919bbf
stephentyers1975 <steve.tyers@gmail.com>
2025-04-14 14:50:01 +0100

    updated kube config

--- src/terraform/k8s/versions.tf
+++ src/terraform/k8s/versions.tf
@@ -1,12 +1,17 @@
 terraform {
   required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
-    }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.75.0"
+      # version = "~> 3.75.0"
+      # version = "~> 4.26.0"
+      version = "~>3.0"
+      
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 3.3.0"
+      # version = "~> 2.45.0"
+      # version = "~> 2.7.0"
     }
   }
   backend "azurerm" {
