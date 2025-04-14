a6dee4b1ea91b39c5931420e3d3772ee92485c78
stephentyers1975 <steve.tyers@gmail.com>
2025-04-14 15:43:28 +0100

    edited providers

--- src/terraform/k8s/versions.tf
+++ src/terraform/k8s/versions.tf
@@ -31,8 +31,6 @@ provider "kubernetes" {
   client_certificate = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
   client_key         = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
   cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
-
- 
 }
 
 data "azurerm_kubernetes_cluster" "aks" {
